07febe0bf6be0bac9599542138e05a41be05fd48
kusuriuri <kate.hamilton@applango.com>
2015-08-07 10:45:34 -0500

    always a tweak

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -8,9 +8,9 @@ app.controller('homeController', ['$scope',
         // not introducing additional filter/orderby, mostly b/c of time crunch
         // angular can get picky when it comes to some things, ugh
         // $scope.order('-title',false);
-        // $scope.order = function(predicate, reverse) {
-        //     $scope.results2 = orderBy($scope.results2, predicate, reverse);
-        // };
+        $scope.order = function(predicate) {
+            $scope.results2 = orderBy($scope.results2, predicate, false);
+        };
 
     }
 ]);
