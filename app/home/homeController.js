7afa57a80afd3f061235d63cb3d192d7b1a2c355
kusuriuri <kate.hamilton@applango.com>
2015-08-07 10:42:51 -0500

    last push

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -1,9 +1,16 @@
+/* jshint undef:true, unused:true, latedef:false */
+/* global app */
 
 app.controller('homeController', ['$scope',
-    function ($scope) {
+    function($scope){
         'use strict';
 
-        $scope.yes = true;
+        // not introducing additional filter/orderby, mostly b/c of time crunch
+        // angular can get picky when it comes to some things, ugh
+        // $scope.order('-title',false);
+        // $scope.order = function(predicate, reverse) {
+        //     $scope.results2 = orderBy($scope.results2, predicate, reverse);
+        // };
 
     }
 ]);
--- app/shared/userFactory.js
+++ app/shared/userFactory.js
@@ -1,4 +1,4 @@
-/* jshint undef:true, unused:true, latedef:false */
+/* jshint undef:true, unused:false, latedef:false */
 /* global app, getdatafunction */
 
 app.factory('userFactory', ['$http',
@@ -9,7 +9,6 @@ app.factory('userFactory', ['$http',
             results1 = [],
             results2 = [];
 
-        //will throw error b/c no promise; not as much of issue in prototype, def issue in prod
         $http.get('https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=a5e95177da353f58113fd60296e1d250&user_id=132365033@N08&format=json&nojsoncallback=1')
         .then(function(res1){
             var data1 = res1.data.photos.photo;
@@ -17,6 +16,7 @@ app.factory('userFactory', ['$http',
             // and then do call for anything additional... still a lot of calls, tho. in prod, would create proxy & cache, rather than
             // repeat 60+ calls for each page, ugh
             for (var i=0; i<21; i++) {
+                // will throw error b/c no promise; not as much of issue in prototype, def issue in prod
                 var obj = {
                     num: i,
                     id: data1[i].id,
@@ -35,20 +35,18 @@ app.factory('userFactory', ['$http',
         });
 
         function getData(num, id) {
-            // in prod, probably run $q and hit proxy, in protoype, simple chaining is quicker
+            // in prod, probably run $q and hit proxy, in protoype, simple chaining is quicker &
             // more readable, while working out what is most efficient for retrieving data
-            $http.get('https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=a5e95177da353f58113fd60296e1d250&user_id=132365033@N08&format=json&nojsoncallback=1&photo_id='+ id)
+
+            $http.get('https://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key=a5e95177da353f58113fd60296e1d250&user_id=132365033@N08&format=json&nojsoncallback=1&photo_id='+ id)
             .then(function(res2){
                 var data2 = res2.data.photo;
                 var obj = {
-                    views: data2.views,
-                    date: data2.dateuploaded,
-                    fave: data2.isfavorite,
-                    username: data2.owner.username
+                    camera: data2.camera
                 };
                 results2.push(obj);
 
-                return $http.get('https://api.flickr.com/services/rest/?method=flickr.photos.getExif&api_key=a5e95177da353f58113fd60296e1d250&user_id=132365033@N08&format=json&nojsoncallback=1&photo_id='+ id);
+                return $http.get('https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=a5e95177da353f58113fd60296e1d250&user_id=132365033@N08&format=json&nojsoncallback=1&photo_id='+ id);
                 }).then(function(res3){
                     var data3 = res3.data.photo;
                     var obj = {
@@ -58,9 +56,11 @@ app.factory('userFactory', ['$http',
                         farm: results1[num].farm,
                         secret: results1[num].secret,
                         server: results1[num].server,
-                        info: results2[num],
-                        camera: data3.camera,
-                        addinfo: data3.exif
+                        views: data3.views,
+                        date: data3.dateuploaded,
+                        fave: data3.isfavorite,
+                        username: data3.owner.username,
+                        camera: results2[num].camera
                     };
                     factory.push(obj);
                 });
